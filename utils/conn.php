a5926e7788ed9aab94f2129fe2e2880a2cceace9
ZHAO YIRAN <yiran.zhao@ucdconnect.ie>
2021-03-09 17:26:22 +0800

    1

---
+++ utils/conn.php
@@ -0,0 +1,13 @@
+<?php
+$conn = mysqli_connect("localhost", "rswy", "a1b2c3hj");
+
+if (!$conn) {
+    die("数据库链接失败!");
+}
+
+//2、设置操作的数据库===================================
+mysqli_select_db($conn, "rswy"); //设置操作的数据库
+
+
+//3、设置操作编码=======================================
+mysqli_query($conn, "set names utf8");
