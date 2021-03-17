f76da812c930ddc5b5d46ad3befd117d223caa72
Zhao Yiran <ejanquesheng@gmail.com>
2021-03-17 23:17:06 +0800

    personal-information

--- utils/file-upload.php
+++ utils/file-upload.php
@@ -2,17 +2,27 @@
 session_start();
 include('./conn.php');
 
+if (!isset($_FILES['myfile'])) {
+    echo 200;
+    exit;
+}
+
 $type = $_POST['type'];
 $imgname = $_FILES['myfile']['name'];
 $tmp = $_FILES['myfile']['tmp_name'];
 $filepath = $_SERVER['DOCUMENT_ROOT'];
+//$filepath = "";
+
+$sql = "SELECT * FROM user WHERE username=" . $_SESSION['username'];
+$user = mysqli_fetch_assoc(mysqli_query($conn, $sql));
+$id = $user['id'];
 
 $filepathIndex = array(
-    "header" => "/images/user/header/"
+    "header" => "/images/user/header/" . $id . "/"
 );
 
 $sqlStatementIndex = array(
-    "header" => "UPDATE user SET header='" . $filepath . $imgname . "' WHERE username='" . $_SESSION['username'] . "'"
+    "header" => "UPDATE user SET header='" . $filepathIndex[$type] . $imgname . "' WHERE username='" . $_SESSION['username'] . "'"
 );
 
 //$returnHrefIndex = array(
@@ -21,16 +31,19 @@ $sqlStatementIndex = array(
 
 $filepath = $filepath . $filepathIndex[$type];
 
+if (!is_dir($filepath)) {
+    mkdir($filepath);
+}
 
 if (move_uploaded_file($tmp, $filepath . $imgname)) {
     $sql = $sqlStatementIndex[$type];
     if ($conn->query($sql) === TRUE) {
         echo 100;
     } else {
-        echo 200;
+        echo 300;
     }
 } else {
-    echo 300;
+    echo 400;
 }
 
 mysqli_close($conn);
