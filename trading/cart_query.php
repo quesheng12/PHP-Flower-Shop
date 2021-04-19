3d6179f8c7758b2e1bf76f85d6d9c2fa02ea3241
Zhao Yiran <ejanquesheng@gmail.com>
2021-04-19 11:13:06 +0800

    quick view function

--- trading/cart_query.php
+++ trading/cart_query.php
@@ -1,23 +1,24 @@
 <?php
-
-include ('../utils/conn.php');
+session_start();
+include('../utils/conn.php');
 
 $p = $_POST;
+//使userid为session中的id
+$p['user_id'] = $_SESSION['uid'];
 
-if($p['action'] == 1){
+if ($p['action'] == 1) {
     show_cart($conn, $p['user_id']);
-}
-else if($p['action'] == 2){
+} else if ($p['action'] == 2) {
     add_to_cart($conn, $p['user_id'], $p['item_id'], $p['quantity']);
-}
-else if($p['action'] == 3){
+} else if ($p['action'] == 3) {
     delete_from_cart($conn, $p['user_id'], $p['item_id']);
 }
 
 mysqli_close($conn);
 
 //获取购物车所以商品 返回json
-function show_cart($conn, $user_id){
+function show_cart($conn, $user_id)
+{
     try {
         $sql = "select item_id, name, image, price, stock from cart natural join item where user_id = " . $user_id;
         $rst = mysqli_query($conn, $sql);
@@ -32,37 +33,38 @@ function show_cart($conn, $user_id){
         } else {
             echo 300;
         }
-    }catch (Exception $e){
+    } catch (Exception $e) {
         echo 200;
     }
 }
 
 //添加商品到购物车
-function add_to_cart($conn, $user_id, $item_id, $quantity){
-    try{
+function add_to_cart($conn, $user_id, $item_id, $quantity)
+{
+    try {
         $sql = "insert into cart values (?, ?, ?)";
         $stmt = mysqli_prepare($conn, $sql);
         mysqli_stmt_bind_param($stmt, "iii", $user_id, $item_id, $quantity);
-        if(mysqli_stmt_execute($stmt)){
+        if (mysqli_stmt_execute($stmt)) {
             echo 100;
-        }
-        else {
+        } else {
             $sql = "update cart set quantity = quantity + '$quantity' where user_id = '$user_id' and item_id = '$item_id'";
             mysqli_query($conn, $sql);
             echo 300;
         }
-    }catch (Exception $e){
+    } catch (Exception $e) {
         echo 200;
     }
 }
 
 //从购物车中删除
-function delete_from_cart($conn, $user_id, $item_id){
+function delete_from_cart($conn, $user_id, $item_id)
+{
     try {
         $sql = "delete from cart where user_id = " . $user_id . " and item_id = " . $item_id;
         mysqli_query($conn, $sql);
         echo 100;
-    }catch (Exception $e){
+    } catch (Exception $e) {
         echo 200;
     }
 }
