91a922e92607b712d178ac6f2e0ed881ef5d05ff
Shen Shuyi <shuyi.shen@ucdconnect.ie>
2021-04-19 22:51:40 +0800

    cart.html

--- trading/cart_query.php
+++ trading/cart_query.php
@@ -1,70 +1,87 @@
 <?php
+session_start();
+$uid = $_SESSION['uid'];
 
-include ('../utils/conn.php');
+include('../utils/conn.php');
 
 $p = $_POST;
 
-if($p['action'] == 1){
-    show_cart($conn, $p['user_id']);
-}
-else if($p['action'] == 2){
-    add_to_cart($conn, $p['user_id'], $p['item_id'], $p['quantity']);
-}
-else if($p['action'] == 3){
-    delete_from_cart($conn, $p['user_id'], $p['item_id']);
+if ($p['action'] == 1) {
+    show_cart($conn, $uid);
+} else if ($p['action'] == 2) {
+    add_to_cart($conn, $uid, $p['item_id'], $p['quantity']);
+} else if ($p['action'] == 3) {
+    delete_single($conn, $uid, $p['item_id']);
+} else if ($p['action'] == 4) {
+    delete_all($conn, $uid);
 }
 
 mysqli_close($conn);
 
 //获取购物车所以商品 返回json
-function show_cart($conn, $user_id){
+function show_cart($conn, $user_id)
+{
     try {
-        $sql = "select item_id, name, image, price, stock from cart natural join item where user_id = " . $user_id;
+        $sql = "select item_id, name, image, price, quantity, stock from cart inner join item on cart.item_id = item.id where user_id = " . $user_id;
         $rst = mysqli_query($conn, $sql);
-        if (mysqli_num_rows($rst) > 0) {
-            $data = '{"items": [';
-            while ($arr = mysqli_fetch_assoc($rst)) {
-                $data = $data . '{"item_id": ' . $arr['item_id'] . ', "name": ' . $arr['name'] . ', "image": ' . $arr['image'] . ', "price": ' . $arr['price'] . ', "stock": ' . $arr['stock'] . '}, ';
-            }
-            $data = chop($data, ", ");
-            $data = $data . ']}';
-            echo json_encode($data);
-        } else {
-            echo 300;
+        $num = 1;
+        $data = array();
+        while ($arr = mysqli_fetch_assoc($rst)) {
+            $node = array(
+                "id" => $arr['item_id'],
+                "name" => $arr['name'],
+                "image" => $arr['image'],
+                "price" => $arr['price'],
+                "quantity" => $arr['quantity'],
+                "stock" => $arr['stock']
+            );
+            $data[$num] = $node;
+            $num++;
         }
-    }catch (Exception $e){
+        echo json_encode($data);
+    } catch (Exception $e) {
         echo 200;
     }
 }
 
 //添加商品到购物车
-function add_to_cart($conn, $user_id, $item_id, $quantity){
-    try{
+function add_to_cart($conn, $user_id, $item_id, $quantity)
+{
+    try {
         $sql = "insert into cart values (?, ?, ?)";
         $stmt = mysqli_prepare($conn, $sql);
         mysqli_stmt_bind_param($stmt, "iii", $user_id, $item_id, $quantity);
-        if(mysqli_stmt_execute($stmt)){
+        if (mysqli_stmt_execute($stmt)) {
             echo 100;
-        }
-        else {
+        } else {
             $sql = "update cart set quantity = quantity + '$quantity' where user_id = '$user_id' and item_id = '$item_id'";
             mysqli_query($conn, $sql);
             echo 300;
         }
-    }catch (Exception $e){
+    } catch (Exception $e) {
         echo 200;
     }
 }
 
 //从购物车中删除
-function delete_from_cart($conn, $user_id, $item_id){
+function delete_single($conn, $user_id, $item_id)
+{
     try {
         $sql = "delete from cart where user_id = " . $user_id . " and item_id = " . $item_id;
         mysqli_query($conn, $sql);
         echo 100;
-    }catch (Exception $e){
+    } catch (Exception $e) {
         echo 200;
     }
 }
 
-
+function delete_all($conn, $user_id)
+{
+    try {
+        $sql = "delete from cart where user_id = " . $user_id;
+        mysqli_query($conn, $sql);
+        echo 100;
+    } catch (Exception $e) {
+        echo 200;
+    }
+}
