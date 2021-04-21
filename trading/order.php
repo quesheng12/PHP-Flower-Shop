c99f1dcacd0a1e7c06db9b7a23ec43b817dbb7ef
Shen Shuyi <shuyi.shen@ucdconnect.ie>
2021-04-21 20:36:26 +0800

    add checkout.php which can generate order.

--- trading/order.php
+++ trading/order.php
@@ -2,12 +2,12 @@
 session_start();
 $uid = $_SESSION['uid'];
 
-include ('../utils/conn.php');
+include('../utils/conn.php');
 
 $p = $_POST;
 
 if($p['action'] == 1){
-    orders($conn, $p['user_id'], $p['item_id'], $p['quantity'], $p['service'], $p['address'], $p['tel']);
+    order_item($conn, orders($conn, $uid, $p['service'], $p['address'], $p['note'], $p['status']), $p['item'], $p['quantity']);
 }
 
 mysqli_close($conn);
@@ -15,14 +15,27 @@ mysqli_close($conn);
 //结算
 function orders($conn, $user_id, $service, $address, $note, $status){
     try{
-        $sql = "insert into orders user_id, service, address_id, note, status, time values ('$user_id', '$service', '$address', '$note', '$status', NOW())";
+        $sql = "insert into orders (user_id, service, address_id, note, status, time) values ('$user_id', '$service', '$address', '$note', '$status', NOW())";
         mysqli_query($conn, $sql);
-        echo 100;
+        $sql = "select id, time from orders where user_id = '$user_id' order by time desc limit 1";
+        $rst = mysqli_query($conn, $sql);
+        $arr = mysqli_fetch_assoc($rst);
+        return $arr['id'];
     }catch (Exception $e){
         echo 200;
     }
 }
 
-function order_query(){
-
+function order_item($conn, $oid, $item, $quantity){
+    try{
+        for($i=0;$i<count($item);$i++){
+            $iid = $item[$i];
+            $q = $quantity[$i];
+            $sql = "insert into order_item values ('$oid', '$iid', '$q')";
+            mysqli_query($conn, $sql);
+        }
+        echo 100;
+    }catch (Exception $e){
+        echo 200;
+    }
 }
